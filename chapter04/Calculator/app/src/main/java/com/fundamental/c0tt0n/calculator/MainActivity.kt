aa4ceaaa1b63c3a68f331d3bb907a3c6f9a87247
yokubo <utsue.38@gmail.com>
2018-07-02 22:29:16 +0900

    create main screen

--- chapter04/Calculator/app/src/main/java/com/fundamental/c0tt0n/calculator/MainActivity.kt
+++ chapter04/Calculator/app/src/main/java/com/fundamental/c0tt0n/calculator/MainActivity.kt
@@ -1,12 +1,39 @@
 package com.fundamental.c0tt0n.calculator
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.fundamental.c0tt0n.calculator.R.string
+import kotlinx.android.synthetic.main.activity_main.calculate
+import kotlinx.android.synthetic.main.activity_main.discountEdit
+import kotlinx.android.synthetic.main.activity_main.priceEdit
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    calculate.setOnClickListener {
+      var isValid = true
+
+      val priceText = priceEdit.text.toString()
+      if (priceText.isEmpty()) {
+        priceEdit.error = getString(string.price_error)
+        isValid = false
+      }
+
+      val discountText = discountEdit.text.toString()
+      if (discountText.isEmpty()) {
+        discountEdit.error = getString(string.discount_error)
+        isValid = false
+      }
+
+      if (isValid) {
+        val price = Integer.parseInt(priceText)
+        val discount = Integer.parseInt(discountText)
+
+        // TODO: transit to ResultActivity
+      }
+    }
   }
 }
