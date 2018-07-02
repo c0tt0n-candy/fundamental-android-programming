33618a8d3cbe1fa8cb2bba5733e07862ad42e019
yokubo <utsue.38@gmail.com>
2018-07-02 23:12:34 +0900

    calculate and show result in ResultActivity

--- chapter04/Calculator/app/src/main/java/com/fundamental/c0tt0n/calculator/ResultActivity.kt
+++ chapter04/Calculator/app/src/main/java/com/fundamental/c0tt0n/calculator/ResultActivity.kt
@@ -4,12 +4,21 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_result.expressionLabel
+import kotlinx.android.synthetic.main.activity_result.resultLabel
 
 class ResultActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_result)
+
+    val price = intent.getIntExtra(EXT_PRICE, 0)
+    val discount = intent.getIntExtra(EXT_DISCOUNT, 0)
+    expressionLabel.text = getString(R.string.expression_label, price, discount)
+
+    val discountPrice = price * (100 - discount) / 100
+    resultLabel.text = getString(R.string.result_label, discountPrice)
   }
 
   companion object {
