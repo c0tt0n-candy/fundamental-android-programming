c2e806c11e1dc2b989f951fba964040af3a083c3
yokubo <utsue.38@gmail.com>
2018-07-03 21:17:01 +0900

    implement transition from Main to TimeZoneSelect

--- chapter05/WorldClock/app/src/main/java/com/fundamental/c0tt0n/worldclock/MainActivity.kt
+++ chapter05/WorldClock/app/src/main/java/com/fundamental/c0tt0n/worldclock/MainActivity.kt
@@ -2,6 +2,7 @@ package com.fundamental.c0tt0n.worldclock
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.add
 import java.util.TimeZone
 import kotlinx.android.synthetic.main.activity_main.timeZone as timeZoneView
 
@@ -13,5 +14,13 @@ class MainActivity : AppCompatActivity() {
 
     val timeZone = TimeZone.getDefault()
     timeZoneView.text = timeZone.displayName
+
+    add.setOnClickListener {
+      startActivityForResult(TimeZoneSelectActivity.getIntent(this), REQUEST_CODE)
+    }
+  }
+
+  companion object {
+    private const val REQUEST_CODE = 1
   }
 }
--- chapter05/WorldClock/app/src/main/java/com/fundamental/c0tt0n/worldclock/TimeZoneSelectActivity.kt
+++ chapter05/WorldClock/app/src/main/java/com/fundamental/c0tt0n/worldclock/TimeZoneSelectActivity.kt
@@ -1,12 +1,18 @@
 package com.fundamental.c0tt0n.worldclock
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class TimeZoneSelectActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_time_zone_select)
+  }
+
+  companion object {
+    fun getIntent(context: Context) = Intent(context, TimeZoneSelectActivity::class.java)
   }
 }
