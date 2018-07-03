7a174e0e0964501e1382e120ba6104fbed84192b
yokubo <utsue.38@gmail.com>
2018-07-03 22:07:56 +0900

    implement TimeZoneSelectActivity

--- chapter05/WorldClock/app/src/main/java/com/fundamental/c0tt0n/worldclock/TimeZoneSelectActivity.kt
+++ chapter05/WorldClock/app/src/main/java/com/fundamental/c0tt0n/worldclock/TimeZoneSelectActivity.kt
@@ -1,18 +1,34 @@
 package com.fundamental.c0tt0n.worldclock
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_time_zone_select.clockList
 
 class TimeZoneSelectActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_time_zone_select)
+
+    setResult(Activity.RESULT_CANCELED)
+
+    clockList.apply {
+      val adapter = TimeZoneAdapter(this@TimeZoneSelectActivity)
+      this.adapter = adapter
+      setOnItemClickListener { _, _, position, _ ->
+        val timeZone = adapter.getItem(position)
+        setResult(Activity.RESULT_OK, Intent().putExtra(EXT_TIME_ZONE, timeZone))
+        finish()
+      }
+    }
   }
 
   companion object {
+    const val EXT_TIME_ZONE = "ext_time_zone"
+
     fun getIntent(context: Context) = Intent(context, TimeZoneSelectActivity::class.java)
   }
 }
