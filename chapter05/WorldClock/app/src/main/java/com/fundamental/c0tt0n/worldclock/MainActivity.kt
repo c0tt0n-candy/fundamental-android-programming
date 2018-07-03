fef8f8e6751ccb8915c5fe1b3fcc32ed572ce6f7
yokubo <utsue.38@gmail.com>
2018-07-03 21:11:28 +0900

    create main screen

--- chapter05/WorldClock/app/src/main/java/com/fundamental/c0tt0n/worldclock/MainActivity.kt
+++ chapter05/WorldClock/app/src/main/java/com/fundamental/c0tt0n/worldclock/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.fundamental.c0tt0n.worldclock
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import java.util.TimeZone
+import kotlinx.android.synthetic.main.activity_main.timeZone as timeZoneView
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    val timeZone = TimeZone.getDefault()
+    timeZoneView.text = timeZone.displayName
   }
 }
